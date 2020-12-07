856d2e622c86da563e86dee308cd81b10728cc1e
Denis-Ozolin <ozolindenx@gmail.com>
2020-12-08 00:32:36 +0200

    fixed task-6.

--- JS/task-6.js
+++ JS/task-6.js
@@ -1,27 +1,13 @@
-
 let total = 0;
 let input = '';
 
 do {
-    input = prompt('Введите число'); 
-} while (input === !null) {
-    // let input = prompt('Введите число'); 
-    
-    // if (input === null) {
-    //     break
-    // } 
-
-    input = Number(input);
-
-    if (Number.isNaN(input)) {
+    input = prompt('Введите число');
+    if (Number.isNaN(Number(input))) {
         alert('Было введено не число, попробуйте еще раз');
     } else {
-        total += input;
+        total += Number(input);
     }
-
-}  
-
+} while (input !== null) {
     alert(`Общая сумма чисел равна ${total}`);
-
-
-
+}
